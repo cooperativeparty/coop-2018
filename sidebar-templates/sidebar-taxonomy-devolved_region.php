48b80e933498cd118a80ddb4605b3de4b4fd5e29
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-08-02 17:50:24 +0100

    Devolved sidebar

--- sidebar-templates/sidebar-taxonomy-devolved_region.php
+++ sidebar-templates/sidebar-taxonomy-devolved_region.php
@@ -1,12 +1,12 @@
 <div class="col-lg-4 widget-area" id="secondary" role="complementary">
     <?php get_template_part( 'partials/point', 'person' );?>
-        <div id="Events" class="widget p-2 my-2">
+        <div id="Events" class="widget p-3 my-3">
             <?php  get_template_part( 'partials/listings', 'events' );?>
         </div>
-        <div id="publications" class="widget widget_categories p-2 my-2">
+        <div id="publications" class="widget widget_categories p-3 my-3">
             <?php  get_template_part( 'partials/listings', 'publications' );?>
         </div>
-        <div id="" class="p-2 my-2 widget">
+        <div id="" class="p-3 my-3 widget">
             <?php  get_template_part( 'partials/listings', 'people' );?>
         </div>
         <?php dynamic_sidebar( 'sidebar-all' ); ?>
