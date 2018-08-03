c5f430593c077652c6965b9660ee753cb23b2233
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-08-03 12:03:40 +0100

    Lost password

--- partials/login-form.php
+++ partials/login-form.php
@@ -23,7 +23,7 @@
             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
-                        <div class="modal-header">
+                        <div class="modal-header bg-purple text-white">
                             <h5 class="modal-title" id="exampleModalLabel">Co-operative Party login</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
                         </div>
