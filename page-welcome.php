b3dd7c866b1af3ee277f2f292abc6fcc79cb0b7a
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-08-08 16:21:22 +0100

    Add modal

--- page-welcome.php
+++ page-welcome.php
@@ -12,9 +12,24 @@ $container   = get_theme_mod( 'understrap_container_type' );
                 <div id="primary" class="content-area col py-5">
                     <main class="site-main" id="main">
                         <?php while ( have_posts() ) : the_post(); ?> Test
-                            <?php get_template_part( 'loop-templates/content', 'page' );
-                        get_template_part('partials/cta', 'box') ;   ?>
-                                <?php endwhile; // end of the loop. ?>
+                            <div class="modal" tabindex="-1" role="dialog">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Modal title</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Modal body text goes here.</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php endwhile; // end of the loop. ?>
                     </main>
                     <!-- #main -->
                 </div>
