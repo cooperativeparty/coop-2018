e00aa40fbc624b078622299e6f6e001943673a22
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-08-08 16:23:49 +0100

    Message

--- page-welcome.php
+++ page-welcome.php
@@ -16,7 +16,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title">Modal title</h5>
+                                            <h5 class="modal-title">Please Log in</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
                                         </div>
                                         <div class="modal-body">
