8ebcd06676ecd99e25d7498d19344a9465db496c
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-10-03 11:21:08 +0100

    Bootstrap classes

--- content-cards.php
+++ content-cards.php
@@ -1,16 +1,17 @@
-<div class="<?php the_cc_css_classes( 'content_cards_card card' ); ?>">
-    <?php if ( get_cc_data( 'image' ) ) : ?>
-        <div class="content_cards_image"> <a class="content_cards_image_link" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>>
+<div class="<?php the_cc_css_classes( 'card' ); ?>">
+    <?php if ( get_cc_data( 'image' ) ) : ?> <a class="card-img-top" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>>
 					<?php the_cc_image( 'full' ); ?>
-				</a> </div>
+				</a>
         <?php endif; ?>
-            <div class="content_cards_title"> <a class="content_cards_title_link" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>>
+            <div class="card-body">
+                <h5 class="card-title"> <a class="content_cards_title_link" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>>
 			<?php the_cc_data( 'title' ); ?>
-		</a> </div>
-            <div class="content_cards_description"> <a class="content_cards_description_link" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>>
+		</a> </h5>
+                <p class="card-text"> <a class="content_cards_description_link" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>>
 			<?php the_cc_data( 'description' ); ?>
-		</a> </div>
-            <div class="content_cards_site_name">
+                </a> </p>
+            </div>
+            <div class="card-footer">
                 <?php if ( get_cc_data('favicon') ) : ?><img src="<?php the_cc_data( 'favicon', 'esc_url' ); ?>" alt="<?php the_cc_data( 'site_name', 'esc_attr' ); ?>" class="content_cards_favicon" />
                     <?php endif; ?>
                         <?php the_cc_data( 'site_name' ); ?>
