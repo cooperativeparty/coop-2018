888cf7b6b1fbe5999dd7c3a44f9f06f80a52b6cb
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-10-03 11:29:14 +0100

    Small

--- content-cards.php
+++ content-cards.php
@@ -5,11 +5,9 @@
                 <div class="card-body">
                     <h5 class="card-title"> 
 			<?php the_cc_data( 'title' ); ?>
-		</h5>
-                    <p class="card-text text-muted">
+		</h5> <small class="card-text text-muted">
                         <?php the_cc_data( 'description' ); ?>
-                    </p>
-                </div>
+                    </small> </div>
                 <div class="card-footer">
                     <?php if ( get_cc_data('favicon') ) : ?><img src="<?php the_cc_data( 'favicon', 'esc_url' ); ?>" alt="<?php the_cc_data( 'site_name', 'esc_attr' ); ?>" class="content_cards_favicon" />
                         <?php endif; ?> <a class="card-link" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>> <?php the_cc_data( 'site_name' ); ?></a> </div>
