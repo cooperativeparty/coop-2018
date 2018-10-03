ae5d3a9ef0356768686e4f64631266c3112bbf35
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-10-03 11:24:28 +0100

    Bootstrap tweaks

--- content-cards.php
+++ content-cards.php
@@ -1,19 +1,16 @@
 <div class="<?php the_cc_css_classes( 'card' ); ?>">
-    <?php if ( get_cc_data( 'image' ) ) : ?> <a class="card-img-top" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>>
-					<?php the_cc_image( 'full' ); ?>
-				</a>
-        <?php endif; ?>
-            <div class="card-body">
-                <h5 class="card-title"> 
+    <?php if ( get_cc_data( 'image' ) ) : ?>
+        <?php the_cc_image( 'full' ); ?>
+            <?php endif; ?>
+                <div class="card-body">
+                    <h5 class="card-title"> 
 			<?php the_cc_data( 'title' ); ?>
 		</h5>
-                <p class="card-text">
-                    <?php the_cc_data( 'description' ); ?>
-                </p>
-            </div>
-            <div class="card-footer">
-                <?php if ( get_cc_data('favicon') ) : ?><img src="<?php the_cc_data( 'favicon', 'esc_url' ); ?>" alt="<?php the_cc_data( 'site_name', 'esc_attr' ); ?>" class="content_cards_favicon" />
-                    <?php endif; ?>
-                        <?php the_cc_data( 'site_name' ); ?>
-            </div>
+                    <p class="card-text text-muted">
+                        <?php the_cc_data( 'description' ); ?>
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <?php if ( get_cc_data('favicon') ) : ?><img src="<?php the_cc_data( 'favicon', 'esc_url' ); ?>" alt="<?php the_cc_data( 'site_name', 'esc_attr' ); ?>" class="content_cards_favicon" />
+                        <?php endif; ?> <a class="card-link" href="<?php the_cc_data( 'url', 'esc_url' ); ?>" <?php the_cc_target(); ?>> <?php the_cc_data( 'site_name' ); ?></a> </div>
 </div>
