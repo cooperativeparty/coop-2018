629dbc977b22ff5e1edd54faab5f39f1e8dcba78
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-11-08 14:46:16 +0000

    Remove manual addition of Twitter Code from homepage

--- homepage.php
+++ homepage.php
@@ -32,22 +32,6 @@ get_template_part( 'includes/page', 'banner' );
                             </div>
                         </div>
                     </div>
-                    <script type="text/javascript">
-                        window.twttr = (function (d, s, id) {
-                            var t, js, fjs = d.getElementsByTagName(s)[0];
-                            if (d.getElementById(id)) return;
-                            js = d.createElement(s);
-                            js.id = id;
-                            js.src = "https://platform.twitter.com/widgets.js";
-                            fjs.parentNode.insertBefore(js, fjs);
-                            return window.twttr || (t = {
-                                _e: []
-                                , ready: function (f) {
-                                    t._e.push(f)
-                                }
-                            });
-                        }(document, "script", "twitter-wjs"));
-                    </script>
                     <div class="col-md-6">
                         <div class="embed-responsive embed-responsive-16by9">
                             <a href="/web/20160707163845/https://party.coop/about">
