4654dd6fd5ae7871f0703257fe070e645407af70
Ben West <42032062+cooperativeparty@users.noreply.github.com>
2018-12-06 13:55:50 +0000

    Remove badge from featured image

--- loop-templates/content-post.php
+++ loop-templates/content-post.php
@@ -23,26 +23,11 @@
                                     <?php get_template_part( 'partials/buttons', 'share' ); ?>
                                 </div>
                             </div>
-                            <hr class="hidden-sm-down" />
-            </header>
+                            <hr class="hidden-sm-down" /> </header>
         </div>
         <div class="row">
             <?php if ( has_post_thumbnail() ) { ?>
                 <figure class=" col-12 figure pb-2 pb-lg-4 img-max post-thumb-wrap hidden-print">
-                    <div class="badge badge-info hidden-sm-down">
-                        <?php         $categories = get_the_category();
-            if ( ! empty( $categories ) ) {
-            echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
-            }
-            $region = wp_get_object_terms( $post->ID, 'devolved_region');
-            if ( ! empty( $region ) ) {
-            echo ' / <a href="' . esc_url( get_term_link( $region[0]->term_id ) ) . '">' . esc_html( $region[0]->name ) . '</a>';
-            }
-             $issues = wp_get_object_terms( $post->ID, 'policyarea');
-            if ( ! empty( $issues ) ) {
-            echo ' / <a href="' . esc_url( get_term_link( $issues[0]->term_id ) ) . '">' . esc_html( $issues[0]->name ) . '</a>';
-            }   
-                ?></div>
                     <?php echo get_the_post_thumbnail( $post->ID, '16by9', array( 'class' => 'img-max' ) ); ?>
                         <figcaption class="figure-caption pt-2">
                             <?php echo get_post(get_post_thumbnail_id())->post_excerpt;?>
@@ -54,9 +39,10 @@
         <div class="row">
             <div class="col-12 col-md-8 <?php echo $toc_class;?> mx-auto">
                 <div class="entry-content">
-                    <?php the_content();?></div>
-            <?php get_template_part('partials/cta', 'box') ;?></div>
-            
+                    <?php the_content();?>
+                </div>
+                <?php get_template_part('partials/cta', 'box') ;?>
+            </div>
             <?php get_template_part('partials/show', 'toc') ;?>
                 <?php /*
 			wp_link_pages( array(
